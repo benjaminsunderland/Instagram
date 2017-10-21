23723d3f0cc42624c424cc31ea933fc5ef3b0499
benjaminsunderland <ben@sunderland.eu.com>
2017-10-21 16:37:40 +0100

    Added controller methods and logic, rendered views accordingly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
---
+++ spec/features/viewing_post_spec.rb
@@ -0,0 +1,14 @@
+# feature "visiting the page to create a post" do
+#   scenario "it has a caption and image upload to create new post" do
+#     createpost
+#     expect(page).to have_css("img[src*='/system/posts/images/000/000/002/medium/doggy.jpg?1508599664']")
+#   end
+# end
+
+def createpost
+  caption = "Doggy Adventure "
+  visit '/posts/new'
+  fill_in "post[caption]", with: caption
+  attach_file("post[image]", Rails.root + "spec/fixtures/doggy.jpg")
+  click_button('Create Post')
+end
